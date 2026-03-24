64f7926c819b193ae44e6d5a72116e096ac09520
cha-linda <linda.chasserieau@gmail.com>
2026-03-24 14:01:53 +0100

    correctif button html

--- app.js
+++ app.js
@@ -71,9 +71,9 @@ document.addEventListener("DOMContentLoaded", () => {
     arows: false,
     dots: true,
     prevArrow:
-      '<button type="button" class="slick-prev"><i class="fas fa-chevron-left"></i></button>',
+      '<button arial-label="vers la gauche" type="button" class="slick-prev"><i class="fas fa-chevron-left"></i></button>',
     nextArrow:
-      '<button type="button" class="slick-next"><i class="fas fa-chevron-right"></i></button>',
+      '<button arial-label="vers la droite" type="button" class="slick-next"><i class="fas fa-chevron-right"></i></button>',
     responsive: [
       {
         breakpoint: 1200,
